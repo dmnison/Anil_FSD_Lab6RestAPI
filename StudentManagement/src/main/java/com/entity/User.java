39705d947671fb6c0e574cb09ce2e4b0342aec7b
dmnison <107767614+dmnison@users.noreply.github.com>
2022-11-10 22:57:04 +0530

    Add files via upload

---
+++ StudentManagement/src/main/java/com/entity/User.java
@@ -0,0 +1,54 @@
+package com.entity;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.*;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+   @Id
+   @Column(name = "user_id")
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+
+   @Column(name="username")
+   private String username;
+   @Column(name="password")
+   private String password;
+      
+   public String getUsername() {
+   	return username;
+   }
+   
+   public void setUsername(String username) {
+   	this.username = username;
+   }
+   
+   public String getPassword() {
+   	return password;
+   }
+   
+   public void setPassword(String password) {
+   	this.password = password;
+   }
+   
+   @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+   @JoinTable(
+           name = "users_roles",
+           joinColumns = @JoinColumn(name = "user_id"),
+           inverseJoinColumns = @JoinColumn(name = "role_id")
+           )
+   private List<Role> roles = new ArrayList<>();
+
+   
+
+
+}
